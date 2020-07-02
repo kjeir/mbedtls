a48af89794dc91f32cd53ca151024493e29e5634
Kjell Eirik Andersen <kjell.andersen@silabs.com>
2020-07-02 17:25:20 +0200

    Added opaque test driver key generation tests.

--- tests/suites/opaque_test_driver.h
+++ tests/suites/opaque_test_driver.h
@@ -1,6 +1,7 @@
 #include "psa/crypto.h"
 
 #define OPAQUE_TEST_DRIVER_KEYHEADER "OPQTDKHEADER"
+#define OPAQUE_TEST_DRIVER_KEYHEADER_SIZE 12U
 
 psa_status_t opaque_test_driver_import_key(const psa_key_attributes_t *attributes,
                                            const uint8_t *in,
