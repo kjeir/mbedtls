67d6623c8eee31156e4731ea68433c741430c20e
Kjell Eirik Andersen <kjell.andersen@silabs.com>
2020-07-02 14:35:09 +0200

    Added opaque test driver key generation API.

--- tests/suites/opaque_test_driver.h
+++ tests/suites/opaque_test_driver.h
@@ -3,8 +3,13 @@
 #define OPAQUE_TEST_DRIVER_KEYHEADER "OPQTDKHEADER"
 
 psa_status_t opaque_test_driver_import_key(const psa_key_attributes_t *attributes,
-                                           const uint8_t *in_key,
-                                           size_t        in_key_len,
-                                           uint8_t       *out_key,
-                                           size_t        out_key_bufsize,
-                                           size_t        *out_key_len);
+                                           const uint8_t *in,
+                                           size_t        in_length,
+                                           uint8_t       *out,
+                                           size_t        out_size,
+                                           size_t        *out_length);
+
+psa_status_t opaque_test_driver_generate_key(const psa_key_attributes_t *attributes,
+                                             uint8_t     *data,
+                                             size_t      data_size,
+                                             size_t      *data_length);
