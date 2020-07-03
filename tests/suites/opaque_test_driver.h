48a5622322b44299603340c0c2b0d751365feb7e
Kjell Eirik Andersen <kjell.andersen@silabs.com>
2020-07-03 17:04:57 +0200

    Added opaque test driver public key export API.

--- tests/suites/opaque_test_driver.h
+++ tests/suites/opaque_test_driver.h
@@ -3,14 +3,20 @@
 #define OPAQUE_TEST_DRIVER_KEYHEADER "OPQTDKHEADER"
 #define OPAQUE_TEST_DRIVER_KEYHEADER_SIZE 12U
 
-psa_status_t opaque_test_driver_import_key(const psa_key_attributes_t *attributes,
-                                           const uint8_t *in,
-                                           size_t        in_length,
-                                           uint8_t       *out,
-                                           size_t        out_size,
-                                           size_t        *out_length);
+psa_status_t opaque_test_driver_export_public_key(const uint8_t *in,
+                                                  size_t        in_length,
+                                                  uint8_t       *out,
+                                                  size_t        out_size,
+                                                  size_t        *out_length);
 
 psa_status_t opaque_test_driver_generate_key(const psa_key_attributes_t *attributes,
-                                             uint8_t     *data,
-                                             size_t      data_size,
-                                             size_t      *data_length);
+                                             uint8_t                    *data,
+                                             size_t                     data_size,
+                                             size_t                     *data_length);
+
+psa_status_t opaque_test_driver_import_key(const psa_key_attributes_t *attributes,
+                                           const uint8_t              *in,
+                                           size_t                     in_length,
+                                           uint8_t                    *out,
+                                           size_t                     out_size,
+                                           size_t                     *out_length);
