edcf3c57ce09f7709e2ce7757a65bc92b4cb52aa
Kjell Eirik Andersen <kjell.andersen@silabs.com>
2020-07-21 11:59:06 +0200

    Added license file headers and doxydoc to opaque driver.

--- tests/src/wrapper_opaque_driver.c
+++ tests/src/wrapper_opaque_driver.c
@@ -1,3 +1,28 @@
+/**
+ * \file wrapper_opaque_driver.c
+ *
+ * \brief   This file contains the opaque driver sample implementation.
+ */
+
+/*
+ *  Copyright (C) 2020, ARM Limited, All Rights Reserved
+ *  SPDX-License-Identifier: Apache-2.0
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may
+ *  not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  This file is part of mbed TLS (https://tls.mbed.org)
+ */
+
 #include "wrapper_opaque_driver.h"
 
 #if defined(MBEDTLS_TEST_WRAPPER_OPAQUE_DRIVER_C)
@@ -33,16 +58,16 @@ psa_status_t opaque_driver_export_public_key(const uint8_t *in,
   OPQTD_VALIDATE_RET(out        != NULL);
   OPQTD_VALIDATE_RET(out_length != NULL);
 
-  if (in_length <= OPAQUE_TEST_DRIVER_KEYHEADER_SIZE) {
+  if (in_length <= OPAQUE_DRIVER_KEYHEADER_SIZE) {
     return PSA_ERROR_INVALID_ARGUMENT;
   }
 
-  if (in_length - OPAQUE_TEST_DRIVER_KEYHEADER_SIZE > out_size) {
+  if (in_length - OPAQUE_DRIVER_KEYHEADER_SIZE > out_size) {
     return PSA_ERROR_BUFFER_TOO_SMALL;
   }
 
-  *out_length = in_length - OPAQUE_TEST_DRIVER_KEYHEADER_SIZE;
-  rot13(in + OPAQUE_TEST_DRIVER_KEYHEADER_SIZE, *out_length, out);
+  *out_length = in_length - OPAQUE_DRIVER_KEYHEADER_SIZE;
+  rot13(in + OPAQUE_DRIVER_KEYHEADER_SIZE, *out_length, out);
 
   return PSA_SUCCESS;
 }
@@ -74,7 +99,7 @@ psa_status_t opaque_driver_generate_key(const psa_key_attributes_t *attributes,
     return PSA_ERROR_NOT_SUPPORTED;
   }
 
-  if (OPAQUE_TEST_DRIVER_KEYHEADER_SIZE
+  if (OPAQUE_DRIVER_KEYHEADER_SIZE
       + PSA_BITS_TO_BYTES(psa_get_key_bits(attributes)) > key_size) {
     return PSA_ERROR_BUFFER_TOO_SMALL;
   }
@@ -122,16 +147,16 @@ psa_status_t opaque_driver_import_key(const psa_key_attributes_t *attributes,
     return PSA_ERROR_INVALID_ARGUMENT;
   }
 
-  if (OPAQUE_TEST_DRIVER_KEYHEADER_SIZE + in_length > out_size) {
+  if (OPAQUE_DRIVER_KEYHEADER_SIZE + in_length > out_size) {
     return PSA_ERROR_BUFFER_TOO_SMALL;
   }
 
-  strcpy((char*)out, OPAQUE_TEST_DRIVER_KEYHEADER);
+  strcpy((char*)out, OPAQUE_DRIVER_KEYHEADER);
 
   // Obscure key slightly.
-  rot13(in, in_length, out + OPAQUE_TEST_DRIVER_KEYHEADER_SIZE);
+  rot13(in, in_length, out + OPAQUE_DRIVER_KEYHEADER_SIZE);
 
-  *out_length = in_length + OPAQUE_TEST_DRIVER_KEYHEADER_SIZE;
+  *out_length = in_length + OPAQUE_DRIVER_KEYHEADER_SIZE;
 
   return PSA_SUCCESS;
 }
@@ -158,7 +183,7 @@ psa_status_t opaque_driver_sign_hash(const psa_key_attributes_t *attributes,
   OPQTD_VALIDATE_RET(signature        != NULL);
   OPQTD_VALIDATE_RET(signature_length != NULL);
 
-  if (key_length <= OPAQUE_TEST_DRIVER_KEYHEADER_SIZE) {
+  if (key_length <= OPAQUE_DRIVER_KEYHEADER_SIZE) {
     return PSA_ERROR_INVALID_ARGUMENT;
   }
 
@@ -218,7 +243,7 @@ psa_status_t opaque_driver_verify_hash(const psa_key_attributes_t *attributes,
   OPQTD_VALIDATE_RET(hash       != NULL);
   OPQTD_VALIDATE_RET(signature  != NULL);
 
-  if (key_length <= OPAQUE_TEST_DRIVER_KEYHEADER_SIZE) {
+  if (key_length <= OPAQUE_DRIVER_KEYHEADER_SIZE) {
     return PSA_ERROR_INVALID_ARGUMENT;
   }
 
